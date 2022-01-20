85a96d2a5573f9de09f9496487490c926d5c4296
chandlerblaschka <cblaschka@live.com>
2022-01-19 21:21:42 -0600

    working with suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import { BallTriangle } from 'react-loader-spinner';
+
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<BallTriangle color="#FF00FF" height={200} width={200} />}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
